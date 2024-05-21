4218b5437fe5815a2c87a7b1aa5e7ca290ff3d94
Umeh Chukwuebuka Emmanuel <81016410+Bukson01@users.noreply.github.com>
2024-05-21 23:29:56 +0100

    update

--- text_based_cal.py
+++ text_based_cal.py
@@ -15,3 +15,7 @@ def _calculate(num1, operator, num2):
         return num1 - num2
     elif operator == '*':
         return num1 * num2
+     elif operator == '/':
+        if num2 == 0:
+            raise ZeroDivisionError("Division by zero is not allowed")
+        return num1 / num2
